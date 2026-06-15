54feb57d75cf93b89268b77831c708bd02549a81
Monirul Islam <monirulislammredul@gmail.com>
2026-06-15 08:46:41 +0600

    feat: add BlockCheckoutIfDueExists middleware and update timezone to Asia/Dhaka

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
             'setlocale' => \App\Http\Middleware\SetLocale::class,
+            'checkout.block' => \App\Http\Middleware\BlockCheckoutIfDueExists::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
