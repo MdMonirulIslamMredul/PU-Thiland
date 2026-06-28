b92a49c5eb0c647ad3cd06e595af721a736a4fd6
Monirul Islam <monirulislammredul@gmail.com>
2026-06-28 11:17:28 +0600

    feat: implement localization for blog index page, including title, heading, and read more button

--- resources/views/frontend/blogs/index.blade.php
+++ resources/views/frontend/blogs/index.blade.php
@@ -1,11 +1,11 @@
 @extends('frontend.layouts.app')
 
-@section('title', 'Blog')
+@section('title', __('site.blogs.title'))
 
 @section('content')
     <section class="py-5">
         <div class="container">
-            <h1 class="section-title mb-4">Latest Blogs</h1>
+            <h1 class="section-title mb-4">{{ __('site.blogs.heading') }}</h1>
             <div class="row g-4">
                 @forelse($blogs as $blog)
                     <div class="col-md-6 col-lg-4" data-aos="fade-up">
@@ -17,10 +17,10 @@
                             <div class="card-body d-flex flex-column">
                                 <h5>{{ $blog->title }}</h5>
                                 <p>{{ $blog->excerpt }}</p><a href="{{ route('blogs.show', $blog->slug) }}"
-                                    class="btn btn-outline-dark btn-sm mt-auto">Read More</a>
+                                    class="btn btn-outline-dark btn-sm mt-auto">{{ __('site.blogs.read_more') }}</a>
                             </div>
                         </div>
-                </div>@empty<p>No blog posts available.</p>
+                </div>@empty<p>{{ __('site.blogs.no_posts') }}</p>
                 @endforelse
             </div>
             <div class="mt-4">{{ $blogs->links() }}</div>
